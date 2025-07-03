18553e8a61525cf0ea0b6133677dc6253418994c
Michael Bishop <michael@bishopand.com>
2025-07-03 12:05:28 -0500

    Use physics ray casting for target hits

--- src/targets.rs
+++ src/targets.rs
@@ -1,6 +1,13 @@
 use bevy::prelude::*;
 
-use avian3d::prelude::{ColliderConstructor, ColliderConstructorHierarchy, RigidBody};
+use avian3d::prelude::{
+    ColliderConstructor,
+    ColliderConstructorHierarchy,
+    Dir3,
+    RigidBody,
+    SpatialQuery,
+    SpatialQueryFilter,
+};
 
 use crate::hp_text::{HpText, HpTextPlugin};
 use crate::weapons::Laser;
@@ -8,12 +15,11 @@ use crate::weapons::Laser;
 #[derive(Component)]
 pub struct Target {
     hp: i32,
-    half_extents: Vec3,
 }
 
 impl Target {
-    pub fn new(hp: i32, half_extents: Vec3) -> Self {
-        Self { hp, half_extents }
+    pub fn new(hp: i32) -> Self {
+        Self { hp }
     }
 }
 
@@ -30,8 +36,6 @@ impl Plugin for TargetsPlugin {
     }
 }
 
-const TARGET_HALF_EXTENTS: Vec3 = Vec3::new(0.5, 10.0, 0.5);
-
 fn spawn_target(mut commands: Commands, asset_server: Res<AssetServer>) {
     let scene: Handle<Scene> = asset_server.load("models/targets.glb#Scene0");
     commands
@@ -42,7 +46,7 @@ fn spawn_target(mut commands: Commands, asset_server: Res<AssetServer>) {
             ColliderConstructor::TrimeshFromMesh,
         ))
         .insert(RigidBody::Static)
-        .insert(Target::new(100, TARGET_HALF_EXTENTS));
+        .insert(Target::new(100));
     info!("spawned target with hp 100");
 }
 
@@ -50,28 +54,25 @@ const LASER_DAMAGE: i32 = 5;
 
 fn laser_hit_system(
     mut commands: Commands,
-    mut lasers: Query<(&mut Transform, &mut Laser), Without<Target>>,
-    mut targets: Query<(Entity, &Transform, &mut Target), Without<Laser>>,
+    spatial: SpatialQuery,
+    mut lasers: Query<(Entity, &mut Transform, &mut Laser), Without<Target>>,
+    mut targets: Query<(Entity, &mut Target)>,
     asset_server: Res<AssetServer>,
 ) {
-    for (mut laser_tf, mut laser) in &mut lasers {
-        for (target_entity, target_tf, mut target) in &mut targets {
-            if target.hp <= 0 {
-                continue;
-            }
-            info!(
-                "checking laser at {:?} against target at {:?}",
-                laser_tf.translation, target_tf.translation
-            );
-            let start = laser.prev_position;
-            let end = laser.projected_position;
-            if let Some(hit_pos) = segment_intersects_aabb(
-                start,
-                end,
-                target_tf.translation,
-                target.half_extents,
-            ) {
-                let normal = (hit_pos - target_tf.translation).normalize_or_zero();
+    for (laser_entity, mut laser_tf, mut laser) in &mut lasers {
+        let start = laser.prev_position;
+        let dir_vec = laser_tf.translation - start;
+        let dist = dir_vec.length();
+        if dist <= f32::EPSILON {
+            laser.prev_position = laser_tf.translation;
+            continue;
+        }
+        let dir = Dir3::new_unchecked(dir_vec / dist);
+        let filter = SpatialQueryFilter::default().with_excluded_entities([laser_entity]);
+        if let Some(hit) = spatial.cast_ray(start, dir, dist, true, &filter) {
+            if let Ok((target_entity, mut target)) = targets.get_mut(hit.entity) {
+                let normal = hit.normal;
+                let hit_pos = hit.point;
                 let new_hp = (target.hp - LASER_DAMAGE).max(0);
                 target.hp = new_hp;
                 if new_hp == 0 {
@@ -100,45 +101,8 @@ fn laser_hit_system(
                     (laser.velocity - 2.0 * laser.velocity.dot(normal) * normal)
                         * crate::weapons::LASER_BOUNCE_DECAY;
                 laser_tf.translation = hit_pos;
-                break;
             }
         }
         laser.prev_position = laser_tf.translation;
     }
-}
-
-fn segment_intersects_aabb(
-    start: Vec3,
-    end: Vec3,
-    center: Vec3,
-    half_extents: Vec3,
-) -> Option<Vec3> {
-    let dir = end - start;
-    let axes = [
-        (start.x, dir.x, center.x - half_extents.x, center.x + half_extents.x),
-        (start.y, dir.y, center.y - half_extents.y, center.y + half_extents.y),
-        (start.z, dir.z, center.z - half_extents.z, center.z + half_extents.z),
-    ];
-    let mut tmin: f32 = 0.0;
-    let mut tmax: f32 = 1.0;
-    for (s, d, min, max) in axes {
-        if d.abs() < f32::EPSILON {
-            if s < min || s > max {
-                return None;
-            }
-        } else {
-            let inv_d = 1.0 / d;
-            let mut t1 = (min - s) * inv_d;
-            let mut t2 = (max - s) * inv_d;
-            if t1 > t2 {
-                std::mem::swap(&mut t1, &mut t2);
-            }
-            tmin = tmin.max(t1);
-            tmax = tmax.min(t2);
-            if tmin > tmax {
-                return None;
-            }
-        }
-    }
-    Some(start + dir * tmin)
 }
